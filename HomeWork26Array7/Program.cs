935eef9f824f9ba590485bc2f513755a79c7d490
An-124Rus <111499611+An-124Rus@users.noreply.github.com>
2022-09-17 13:41:43 +0300

    HomeWork26Array7

---
+++ HomeWork26Array7/Program.cs
@@ -0,0 +1,17 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строку из слов: ");
+        string userText = Console.ReadLine();
+
+        string[] wordsString = userText.Split(' ');
+
+        foreach (string word in wordsString)
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
